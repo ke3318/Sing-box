5851a821fecb1fdebef3c2ccef37933b491f9e31
ke3318 <123803104+ke3318@users.noreply.github.com>
2024-08-03 17:24:59 +0800

    Update sb_serv00.sh

--- sb_serv00.sh
+++ sb_serv00.sh
@@ -14,7 +14,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b9'}
+export UUID=${UUID:-'99a91d91-0438-4819-9fbb-11c4a6a0068a'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
